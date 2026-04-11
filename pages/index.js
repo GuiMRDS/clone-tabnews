4648367bdb98babedf01b28d97f4358e59461725
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-04-11 17:11:09 +0000

    Adicionando as paginas e páginas iniciais

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return 
+    <h1>
+        Hello World, meu nome é Guilherme
+
+        Sera que hoje vai ter alguem para min, sera que vai sobrar algo
+
+        Estou no futuro para deixer que não sobrou nesse sabado kkkkk
+    </h1>
+}
+
+export default  Home;
