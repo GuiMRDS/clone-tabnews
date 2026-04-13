ebabe16eb601b17d1c20c25980c39a7692bf6bf3
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-04-13 15:16:42 +0000

    Alteração na frase

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ function Home() {
     <h1>
         Hello World, meu nome é Guilherme
 
-        Sera que hoje vai ter alguem para min, sera que vai sobrar algo
-
-        Estou no futuro para deixer que não sobrou nesse sabado kkkkk
+        Esse é o clone do TabNews
     </h1>
 }
 
