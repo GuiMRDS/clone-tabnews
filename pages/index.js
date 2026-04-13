2f07a36f76fb567330a62c3c244faefd7a8450ca
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-04-13 22:01:59 +0000

    Arrumando a Home e os textos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 function Home() {
-    return 
-    <h1>
-        Hello World, meu nome é Guilherme
+    return (
+        <>
+            <h1>Hello World</h1> 
 
-        Esse é o clone do TabNews
-    </h1>
+            <h3>Quer namorar comigo?</h3>
+
+            <p>
+                Não deu certo, ela não aceitou kkkkk 
+                Mas pelo menos aprendi mais sobre o Vercel e Deploy kkkkkk
+            </p>
+        </>
+    )
 }
 
 export default  Home;
