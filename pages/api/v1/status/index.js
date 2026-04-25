fe2d9f532bb7ee98588c322ce769b7c15ce61f51
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-04-25 21:20:37 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,12 @@
 // status/api
 
-function status(request, reponse) {
-  reponse.status(200).json({ chave: "Quero ser acima da media" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+
+  response.status(200).json({ chave: "Quero ser acima da media" });
 }
 
 export default status;
